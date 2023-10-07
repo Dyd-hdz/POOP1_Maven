9cc6f5bb3961b96a5d6270809ca59abea13859bf
Dyd-hdz <146910278+Dyd-hdz@users.noreply.github.com>
2023-10-07 14:49:50 -0600

    Creación de práctica 01. Proyecto Maven

---
+++ src/main/java/com/mycompany/proyecto_maven/Proyecto_Maven.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_maven;
+
+/**
+ * Proyecto en Maven
+ * @author Luis Hernández
+ */
+public class Proyecto_Maven {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde un proyecto en Mavel");
+        /**
+         * Los proyectos en mavel son mas pesados y mas tardados pero esta garantizado
+         * que el compilador siempre sera el mas actualizado que tiene netbeans ya
+         * que esta conectado a internet y es lo que hace que tarde mas en compilar el codigo
+         */
+    }
+}
